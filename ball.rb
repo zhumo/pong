11bfea92b617fb6fb8d6be931f7a6a53f7638343
zhumo <mozhu888@gmail.com>
2013-11-16 14:49:05 -0500

    made ball move and added scores for both players

--- ball.rb
+++ ball.rb
@@ -1,10 +1,14 @@
 class Ball
-  SIZE = 8
-  attr_reader :x, :y
+  SIZE = 16
+  attr_reader :x, :y, :angle, :speed
 
   def initialize
     @x = Pong::WIDTH/2
     @y = Pong::HEIGHT/2
+
+    @angle = rand(120) + 30
+    @angle *= -1 if rand > 0.5
+    @speed = 6
   end
 
   def x1; @x - SIZE/2; end
@@ -23,6 +27,41 @@ class Ball
       )
   end
 
+  def dx; Gosu.offset_x(angle,speed); end
+  def dy; Gosu.offset_y(angle,speed); end
 
+  def move!
+    @x += dx
+    @y += dy
+
+    if @y < 0
+      @y = 0
+      bounce_off_edge!
+    end
+    if @x < 0
+      @x = 0
+      @angle = Gosu.angle(0,0,-dx,dy)
+    end
+    if @y > Pong::HEIGHT
+      @y = Pong::HEIGHT
+      bounce_off_edge!
+    end
+    if @x > Pong::WIDTH
+      @x = Pong::WIDTH
+      @angle = Gosu.angle(0,0,-dx,dy)
+    end
+
+    def bounce_off_edge!
+      @angle = Gosu.angle(0,0,dx,-dy)
+    end
+  end
+
+  def off_left?
+    x1 < 0
+  end
+
+  def off_right?
+    x2 > Pong::WIDTH
+  end
 
 end
--- pong.rb
+++ pong.rb
@@ -13,10 +13,32 @@ class Pong < Hasu::Window
 
   def reset
     @ball = Ball.new
+
+    @left_score = 0
+    @right_score = 0
+
+    @font = Gosu::Font.new(self,"Arial", 30)
   end
 
   def draw
     @ball.draw(self)
+
+    @font.draw(@left_score,30,30,0)
+    @font.draw(@right_score,WIDTH-50,30,0)
+  end
+
+  def update
+    @ball.move!
+
+    if @ball.off_left?
+      @right_score += 1
+      @ball = Ball.new
+    end
+
+    if @ball.off_right?
+      @left_score += 1
+      @ball = Ball.new
+    end
   end
 end
 
